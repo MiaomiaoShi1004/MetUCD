68ac80024e1bdd78db5b31d76974a2a45ebbe421
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-11-22 20:20:37 +0800

    Initial Commit

---
+++ MetUCD/MetUCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetUCDApp.swift
+//  MetUCD
+//
+//  Created by Miaomiao Shi on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetUCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
